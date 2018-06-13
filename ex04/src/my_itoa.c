91f1af719bf5e305f6015c2f3dc96163afeb1088
Esknor <volodimirvrabets@gmail.com>
2018-06-13 15:02:18 +0300

    I have learned how to ascii

--- ex04/src/my_itoa.c
+++ ex04/src/my_itoa.c
@@ -1,13 +1,41 @@
-//#include "test.h"
+#include "test.h"
 #include<stdio.h>
+#include <stdlib.h>
 
-int my_itoa(int nmb){
-  int a_as_int = (int)'a';
-  return a_as_int;
+char* my_itoa(int nmb)
+{
+    char *str =(char*) malloc(100);
+    char *answ = str;
+    int num = nmb;
+    int j = 1;
+    
+    if (num < 0 ){
+      *str='-';
+       str++;
+       num=-num;
+    };
+
+    while (num > 9) {
+      num = num/10;
+      j=j*10;
+    };
+
+    while (j > 0) {
+      num = nmb / j;
+      *str = '0' + num;
+      str++;
+      nmb -= j * num;
+      j =j/10;
+  }
+  *str = '\0';
+  return answ;
 };
 
-int main(){
-  int output=my_itoa(123);
-  printf("%i", output);
-return 0;
-};
+
+
+
+
+
+
+
+
